86edb6f5737ddfa9a952678f6b890a20f9d40d12
rayhankinan <rayhankinan@gmail.com>
2023-11-27 01:03:37 +0700

    Minor changes

--- generate_clean_mask.m
+++ generate_clean_mask.m
@@ -1,10 +1,7 @@
 % Used only for data cleaning, not for the main application
-function Icleaned = remove_background(I, mask, iterations)
+function bw = generate_clean_mask(I, mask, iterations)
 
 % Generate a mask for the background
 bw = activecontour(I, mask, iterations, "edge");
-
-% Remove the background
-Icleaned = I .* uint8(bw);
 
 end
